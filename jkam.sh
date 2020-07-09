9a3ea621df6d6aa434575dbb0e1ae826eaff7d3f
Júlio Kamizato <julio.kamizato@mmda.com.br>
2020-07-08 22:00:46 -0300

    Add bash script jkam.sh

---
+++ jkam.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+var_function=$1
+var_parameter=$2
+
+
+## functions
+jkam_lando_local_import_db()
+{
+	lando drush sqlc < $var_parameter && lando drush cr && lando drush cim -y && lando drush cr && lando drush uli
+}
+
+## Personal commands
+
+if [ -z "$var_function" ]; then
+	echo "lando-import-db [../backup.sql]"
+fi
+
+if [ "$var_function" == "lando-import-db" ]; then
+	jkam_lando_local_import_db
+fi
