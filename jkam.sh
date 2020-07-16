7d416b14519be3191a53ee9bf0a726fc11376c0f
Júlio <jkamizato>
2020-07-16 11:54:36 -0300

    Add gitlog

--- jkam.sh
+++ jkam.sh
@@ -3,19 +3,38 @@
 var_function=$1
 var_parameter=$2
 
-
 ## functions
 jkam_lando_local_import_db()
 {
-    lando drush sql-drop && lando drush sqlc < $var_parameter && lando drush cr && lando drush cim -y && lando drush updb -y && lando drush cr && lando drush uli
+	lando drush sql-drop -y && \
+	gunzip -c $var_parameter | lando drush sqlc && \
+	lando drush cr && \
+	lando drush cim -y && \
+	lando drush updb -y && \
+	lando drush cr && \
+	lando drush uli
 }
+
+jkam_git_log() {
+	git log --oneline --graph --decorate --all
+}
+
 
 ## Personal commands
 
 if [ -z "$var_function" ]; then
-	echo "lando-import-db [../backup.sql]"
+    echo "lando-import-db [../backup.sql]"
 fi
 
-if [ "$var_function" == "lando-import-db" ]; then
-	jkam_lando_local_import_db
-fi
+
+case "$var_function" in
+	"lando-import-db")
+		jkam_lando_local_import_db
+		;;
+	"git-log")
+		jkam_git_log
+		;;
+	*)
+		;;
+esac
+
